65366cd335058f72319028df382b91e0e30b2c47
seemayadav14 <35358040+seemayadav14@users.noreply.github.com>
2026-07-28 03:37:14 +0530

    Update lambda_function.py

--- Assignment4/lambda_function.py
+++ Assignment4/lambda_function.py
@@ -1,24 +1,20 @@
 import boto3
-from datetime import datetime
 
-ec2 = boto3.client('ec2')
+ec2 = boto3.client("ec2")
 
-# Change these values
-OWNER_ID = "self"
-VOLUME_ID = "vol-xxxxxxxxxxxxxxxxx"
+VOLUME_ID = "vol-0bcc7d80ce744e0c8"
 
 def lambda_handler(event, context):
 
-    # Step 1: Get all snapshots
     snapshots = ec2.describe_snapshots(
-        OwnerIds=[OWNER_ID],
         Filters=[
             {
-                'Name': 'volume-id',
-                'Values': [VOLUME_ID]
+                "Name": "volume-id",
+                "Values": [VOLUME_ID]
             }
-        ]
-    )['Snapshots']
+        ],
+        OwnerIds=["self"]
+    )["Snapshots"]
 
     if not snapshots:
         return {
@@ -26,81 +22,63 @@ def lambda_handler(event, context):
             "body": "No snapshots found."
         }
 
-    # Step 2: Find latest snapshot
     latest_snapshot = sorted(
         snapshots,
-        key=lambda x: x['StartTime'],
+        key=lambda x: x["StartTime"],
         reverse=True
     )[0]
 
-    snapshot_id = latest_snapshot['SnapshotId']
+    snapshot_id = latest_snapshot["SnapshotId"]
 
-    print("Latest Snapshot:", snapshot_id)
-
-    # Step 3: Register AMI
-
-    image_name = f"Restore-{datetime.utcnow().strftime('%Y%m%d-%H%M%S')}"
+    ami_name = f"RestoreAMI-{snapshot_id}"
 
     response = ec2.register_image(
-        Name=image_name,
-        Architecture='x86_64',
-        RootDeviceName='/dev/xvda',
-        VirtualizationType='hvm',
-        EnaSupport=True,
+        Name=ami_name,
+        RootDeviceName="/dev/xvda",
         BlockDeviceMappings=[
             {
-                'DeviceName': '/dev/xvda',
-                'Ebs': {
-                    'SnapshotId': snapshot_id,
-                    'DeleteOnTermination': True,
-                    'VolumeType': 'gp3'
+                "DeviceName": "/dev/xvda",
+                "Ebs": {
+                    "SnapshotId": snapshot_id,
+                    "VolumeSize": latest_snapshot["VolumeSize"],
+                    "VolumeType": "gp3",
+                    "DeleteOnTermination": True
                 }
             }
-        ]
+        ],
+        VirtualizationType="hvm",
+        Architecture="x86_64"
     )
 
-    image_id = response['ImageId']
+    ami_id = response["ImageId"]
 
-    print("AMI:", image_id)
+    waiter = ec2.get_waiter("image_available")
+    waiter.wait(ImageIds=[ami_id])
 
-    # Step 4: Wait until AMI becomes available
-
-    waiter = ec2.get_waiter('image_available')
-    waiter.wait(ImageIds=[image_id])
-
-    print("AMI Available")
-
-    # Step 5: Launch Instance
-
-    launch = ec2.run_instances(
-        ImageId=image_id,
-        InstanceType='t3.micro',
+    instance = ec2.run_instances(
+        ImageId=ami_id,
+        InstanceType="t3.micro",
         MinCount=1,
-        MaxCount=1,
-        TagSpecifications=[
+        MaxCount=1
+    )
+
+    instance_id = instance["Instances"][0]["InstanceId"]
+
+    ec2.create_tags(
+        Resources=[instance_id],
+        Tags=[
             {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'RestoredFrom',
-                        'Value': snapshot_id
-                    },
-                    {
-                        'Key': 'Name',
-                        'Value': 'Restored-Instance'
-                    }
-                ]
+                "Key": "RestoredFrom",
+                "Value": snapshot_id
             }
         ]
     )
 
-    instance_id = launch['Instances'][0]['InstanceId']
-
-    print("New Instance:", instance_id)
+    print("Instance Created:", instance_id)
 
     return {
         "statusCode": 200,
-        "Snapshot": snapshot_id,
-        "AMI": image_id,
-        "Instance": instance_id
+        "InstanceId": instance_id,
+        "SnapshotId": snapshot_id,
+        "AMI": ami_id
     }
